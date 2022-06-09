a156967084c0bdd5c7a5f0fe249851a4bb050a21
joyful-victory <joyfulvictorynkem@gmail.com>
2022-06-09 12:07:29 +0100

    hello

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//using System.Data;
+//using Internal;
+// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.IO;
+using System.Configuration.Assemblies;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace program
+{
+    class  Beauty{
+        static void Main(string[]args)
+        {
+            string fname,lname;
+            Console.Write("Enter your First name: ");
+            fname = Console.ReadLine();
+            Console.Write("Enter your Last name: ");
+            lname = Console.ReadLine();
+            Console.WriteLine("Hi, am joyful welcome to my world!, you are "+fname+" "+lname);
+ 
+        }
+    }
+}
+
